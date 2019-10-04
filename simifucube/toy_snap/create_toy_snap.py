f98a258f49476e8f0b2084b3b9dbc82778c3e24c
Michele Mastropietro <michele.mastropietro@gmail.com>
2019-10-04 14:51:10 +0200

    update create_toysnap

--- simifucube/toy_snap/create_toy_snap.py
+++ simifucube/toy_snap/create_toy_snap.py
@@ -6,7 +6,7 @@ f = pynbody.new(star=2)
 
 mass_prefactor = 1e4
 
-f['pos'] = SimArray(np.array([[-2, 0, 0], [2,0,0]]), units='kpc')
+f['pos'] = SimArray(np.array([[-1, 0, 0], [2,0,0]]), units='kpc', dtype=np.float32)
 f['vel'] = SimArray(np.array([[0, 0, -20],[0,0,500]]), units='km s**-1')
 f['mass'] = SimArray([1e-10,1e-10], units='1e10 Msol') * mass_prefactor
 f.properties['boxsize']=1000 * pynbody.units.kpc
@@ -14,6 +14,8 @@ print(f.properties)
 
 pynbody.config['sph']['smooth-particles'] = 1
 pynbody.config['sph']['tree-leafsize'] = 1
-f['smooth'] = SimArray(np.array([0.2, 0.2]), units='kpc')
+f['smooth'] = SimArray(np.array([1.0, 1.0]), units='kpc')
 
-f.write(filename='toy_snap_d4.0_M1e4', fmt=pynbody.snapshot.gadget.GadgetSnap)
+filename = 'toy_snap_d3_M1e4_sm1'
+f.write(filename=filename, fmt=pynbody.snapshot.gadget.GadgetSnap)
+print(f"Created {filename}")
